7b122b3acd247ee1d5ed8dec3030037963843a36
zhengfeng.yao <zhengfeng.yao@tongdun.cn>
2016-08-30 18:11:09 +0800

    rename test.js

--- test/client/actionTypes.test.js
+++ test/client/actionTypes.test.js
@@ -1,4 +1,8 @@
+/**
+  * Created by Zhengfeng Yao on 16/8/29.
+  */
 import { APP } from 'src/actionTypes';
+
 describe('appEnvConfigTests', () => {
   it('test actionTypes', () => {
     expect(APP.INIT).to.equal('APP.INIT');
